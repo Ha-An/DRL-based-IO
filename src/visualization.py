96e5c56461e2430588f03d4f41ac3b85426e5658
SHJun <junsh0313@hanyang.ac.kr>
2023-08-04 13:09:30 +0900

    sum

--- src/visualization.py
+++ src/visualization.py
@@ -1,42 +1,8 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 '''
-class visualization:
-    def __init__(self, inventory, item_name):
-        self.inventory = inventory
-        self.item_name = item_name
-        
-    
-
-    def inventory_level_graph(self):
-        cont=0
-        sns.set(style="darkgrid")
-        plt.figure(figsize=(10, 6))
-        for inven in self.inventory:
-            
-            plt.plot(self.inventory.level_over_time, label=(' %d : inventory_level',cont))
-            cont=+1
-        plt.xlabel('time[days]')
-        plt.ylabel('inventory')
-        plt.title(f'{self.item_name} inventory_level')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
-    def inventory_cost_graph(self):
-        sns.set(style="darkgrid")
-        plt.figure(figsize=(10, 6))
-        plt.plot(self.inventory.cost_over_time, label='inventory_cost')
-        plt.xlabel('time[days]')
-        plt.ylabel('inventory_cost')
-        plt.title(f'{self.item_name} inventory_cost')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-'''
-
-
 #visualization
 class visualization:
     def __init__(self, inventory):
@@ -59,7 +25,7 @@ class visualization:
         plt.ylabel('inventory')    
         plt.legend() 
         plt.grid(True)
-        plt.show()
+       # plt.show()
        
     def inventory_cost(self,cost,item_name_list):
         sns.set(style="darkgrid")
@@ -70,6 +36,48 @@ class visualization:
         plt.ylabel('inventory')    
         plt.legend() 
         plt.grid(True)
+        #plt.show()
+'''
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class visualization:
+
+    def __init__(self, inventory):
+        self.inventory=inventory
+        self.cost_list=[]    
+        self.level_list=[]
+
+    def return_list(self):
+       self.level_list.append(self.inventory.level_over_time)
+       self.cost_list.append(self.inventory.inventory_cost_over_time)
+       return self.level_list,self.cost_list 
+
+    def plot_inventory_graphs(self, level, cost, item_name_list):
+        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+        
+        # 첫 번째 그래프 (inventory_level)를 왼쪽
+        sns.set(style="darkgrid")
+        for i in range(len(level)):
+            axes[0].plot(level[i][0], label=item_name_list[i])
+        axes[0].set_xlabel('hours')
+        axes[0].set_ylabel('level')
+        axes[0].legend()
+        axes[0].grid(True)
+        axes[0].set_title("inventory level")
+        
+        # 두 번째 그래프 (inventory_cost)를 오른쪽
+        sns.set(style="darkgrid")
+        for i in range(len(cost)):
+            axes[1].plot(cost[i][0], label=item_name_list[i])
+        axes[1].set_xlabel('hours')
+        axes[1].set_ylabel('cost')
+        axes[1].legend()
+        axes[1].grid(True)
+        axes[1].set_title("inventory cost")
+        
+        plt.tight_layout()
         plt.show()
     
     
